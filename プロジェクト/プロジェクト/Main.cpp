db7e94eb7b89d3c4dfcadac7c47f5dfc7063a432
munasuke <34709203+munasuke@users.noreply.github.com>
2019-05-13 18:30:05 +0900

    ライブラリ参照した

--- プロジェクト/プロジェクト/Main.cpp
+++ プロジェクト/プロジェクト/Main.cpp
@@ -1,8 +1,21 @@
+#include <MyLib.h>
+
+#pragma comment(lib, "Lib.lib")
+
 #ifdef _DEBUG
 int main()
 #else
 int __stdcall WinMain(void* hInstance, void* hPrevInstance, char* lpCmdLine, int nShowCmd)
 #endif
 {
+	MyLib lib(640);
+	lib.ChangeTitle("ヒャクエンロジック");
+
+	while (lib.CheckMsg() && !(Input::Get().IsKey(Key::Escape)))
+	{
+		lib.Clear();
+		lib.Execution();
+	}
+
 	return 0;
 }

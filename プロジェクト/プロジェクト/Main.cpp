65bf2238d3f7b489abc874f5e6b9ac1ae39dd3a7
munasuke <34709203+munasuke@users.noreply.github.com>
2019-05-14 10:38:28 +0900

    Revert "Gameクラス作った"
    This reverts commit dccfdcf5d681ad94809845b0a1a2f6e20342f53a.

--- プロジェクト/プロジェクト/Main.cpp
+++ プロジェクト/プロジェクト/Main.cpp
@@ -1,4 +1,7 @@
-#include "Game/Game.h"
+#include <MyLib.h>
+#include <iostream>
+
+#pragma comment(lib, "Lib.lib")
 
 #ifdef _DEBUG
 int main()
@@ -6,8 +9,34 @@ int main()
 int __stdcall WinMain(void* hInstance, void* hPrevInstance, char* lpCmdLine, int nShowCmd)
 #endif
 {
-	Game::Get().Init();
-	Game::Get().Update();
+	MyLib lib(640);
+	lib.ChangeTitle("ヒャクエンロジック");
+	Texture img("img/nino8.jpg");
+
+	while (lib.CheckMsg() && !(Input::Get().IsKey(Key::Escape)))
+	{
+		lib.Clear();
+		lib.Draw(img);
+		lib.Execution();
+
+		if (Input::Get().IsKey(Key::Num4))
+		{
+			img.pos.x--;
+		}
+		if (Input::Get().IsKey(Key::Num6))
+		{
+			img.pos.x++;
+		}
+		if (Input::Get().IsKey(Key::Num8))
+		{
+			img.pos.y--;
+		}
+		if (Input::Get().IsKey(Key::Num2))
+		{
+			img.pos.y++;
+		}
+		std::cout << img.pos.x << ", " << img.pos.y << std::endl;
+	}
 
 	return 0;
 }

7f59b0bcd37cda263755441278ad06173e1317c5
munasuke <34709203+munasuke@users.noreply.github.com>
2019-05-14 10:42:55 +0900

    add

---
+++ プロジェクト/プロジェクト/Scene/Main.cpp
@@ -0,0 +1,26 @@
+#include "Main.h"
+#include <MyLib.h>
+
+// コンストラクタ
+Main::Main(std::weak_ptr<MyLib> lib)
+{
+	this->lib = lib;
+}
+
+// デストラクタ
+Main::~Main()
+{
+}
+
+// 描画
+void Main::Draw(void)
+{
+	lib.lock()->Clear();
+
+	lib.lock()->Execution();
+}
+
+// 処理
+void Main::UpData(void)
+{
+}
